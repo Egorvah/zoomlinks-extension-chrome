11631ba437bdb3180f92dbe517063387da7e2ddb
Egorvah <scsware@yandex.ru>
2013-02-05 15:49:43 +0400

    hotkey

--- js/main.coffee
+++ js/main.coffee
@@ -1,26 +1,31 @@
+
 m = angular.module("zoomlinks", ['ngResource','ui'])
 
 m.factory 'Link', ($resource)->
   $resource 'http://zoomlinks.ru/rest/link/:id'
 
-m.directive "exportBookmarks", ($timeout)->
-  (scope, element, attrs)->
-    angular.element(element).click ->
-      console.log('starting parse bookmarks')
-      chrome.bookmarks.getTree( (BookmarkTreeNode)->
-        console.log(BookmarkTreeNode)
-        #scope.$apply attrs.exportBookmarks
-      )
-
 m.controller 'MainCtr', ($scope, Link)->
   $scope.exportBookmarks = ->
     chrome.bookmarks.getTree( (BookmarkTreeNode)->
-      console.log(BookmarkTreeNode)
-      #scope.$apply attrs.exportBookmarks
+      for tree in BookmarkTreeNode
+        for folder in tree.children
+          for boookmark in folder.children
+            link = new Link()
+            link.url = boookmark.url
+            link.folder = $scope.currentFolder?.id || 0
+            link.$save ->
+
+
     )
 
 
   $scope.exportTabs = ->
     chrome.windows.getAll({"populate" : true}, (tabs)->
-      console.log(tabs)
+      for window in tabs
+        for tab in window.tabs
+          link = new Link()
+          link.url = tab.url
+          link.folder = $scope.currentFolder?.id || 0
+          link.$save ->
+            console.log tab.url
     )
--- js/script.js
+++ js/script.js
@@ -1,51 +1,12 @@
-$(document).ready(function(){
+chrome.extension.sendRequest({localStorage: "hotkey"}, function() {
 
+    //console.log(ctrl,shift,alt,code);
 
-	//ajax send form
-
-    var options = {
-        beforeSubmit:  showRequest,  // pre-submit callback
-        success:       showResponse  // post-submit callback
-    };
-
-    $('#appendLink').ajaxForm(options);
-
-    function showRequest(formData, jqForm, options) {
-
-        return true;
-    }
-
-    function showResponse(responseText, statusText, xhr, $form)  {
-
-            alert(responseText);
-    }
-
-
-	$('.exportBookmarks').click(function(){
-		chrome.bookmarks.getTree(function(BookmarkTreeNode) {
-				for(var i=0;i<BookmarkTreeNode[0].children[0].children.length; i++){
-					console.log(BookmarkTreeNode[0].children[0].children[i].url)
-					$('#url').val(BookmarkTreeNode[0].children[0].children[i].url);
-					//$('#appendLink').ajaxSubmit();
-				}
-			});
-
-		
-	});
-
-	$('.exportTabs').click(function(){
-		chrome.windows.getAll({"populate" : true}, function(windows)
-
-		  {
-
-		    for(var i = 0; i < windows.length; i++)
-		    {
-		      for(var j = 0; j < windows[i].tabs.length; j++)
-		      {
-			        console.log(windows[i].tabs[j].url);
-		       }
-		    }
-		  });
-	});
-
+        document.addEventListener("keydown", function (e){
+        if(e.keyCode == 69 && e.ctrlKey == true && e.shiftKey == true ){   //e.altKey == alt
+            e.preventDefault();
+            chrome.extension.sendRequest({action:"keydown"});
+            return false;
+        }
+    }, false);
 });
