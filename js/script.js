669c0bdf3c4ade48d2e635abe69a18a9681cdc24
Egorvah <scsware@yandex.ru>
2013-02-06 23:46:53 +0400

    Update js/script.js
    hotKey for Mac OS

--- js/script.js
+++ js/script.js
@@ -3,7 +3,8 @@ chrome.extension.sendRequest({localStorage: "hotkey"}, function() {
     //console.log(ctrl,shift,alt,code);
 
         document.addEventListener("keydown", function (e){
-        if(e.keyCode == 69 && e.ctrlKey == true && e.shiftKey == true ){   //e.altKey == alt
+
+        if(e.keyCode == 90 && e.ctrlKey == true && (e.shiftKey == true  || e.metaKey == true) ) { //e.altKey == alt // e.metaKey == mac os command key
             e.preventDefault();
             chrome.extension.sendRequest({action:"keydown"});
             return false;
